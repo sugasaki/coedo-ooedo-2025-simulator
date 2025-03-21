afe1bf431893b6904b0aa0d3975c7188897decad
sugasaki <sugasaki@gmail.com>
2025-03-22 00:02:45 +0900

    DeckGLMapコンポーネントにGeoJSONレイヤーとエイドアイコンレイヤーを追加し、関連データをインポート

--- src/components/DeckGLMap.tsx
+++ src/components/DeckGLMap.tsx
@@ -2,6 +2,12 @@ import { Map } from 'react-map-gl/maplibre';
 import { DeckGLOverlay } from './DeckGLOverlay';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import { useScatterplotLayer } from '../hooks/useScatterplotLayer';
+import { useGeoJsonLayer } from '../hooks/useGeoJsonLayer';
+
+import geojson from '../data/2025-小江戸大江戸v1.3.json';
+import aidJson from '../data/2025-小江戸大江戸-CP.json';
+import { useAidIconLayer } from '../hooks/useAidIconLayer';
+const feature = geojson.features[0];
 
 const mapStyle = `https://basemaps.cartocdn.com/gl/dark-matter-nolabels-gl-style/style.json?key=${
   import.meta.env.VITE_MAPTILER_KEY
@@ -21,13 +27,18 @@ const IMPERIAL_PALACE_LOCATION = {
 
 export const DeckGLMap = ({ width = '100%', height = '500px' }: Props) => {
   const { getScatterplotLayer } = useScatterplotLayer();
+  const { getGeojsonLayer } = useGeoJsonLayer(geojson);
+  const { aidIconLayer } = useAidIconLayer(aidJson, 'aid1', [0, 255, 127], 200);
+  console.log(feature, 'feature');
+
   const scatterplotLayer = getScatterplotLayer();
+  const geojsonLayer = getGeojsonLayer();
 
   const layers = [
     scatterplotLayer,
-    // aidIconLayer, //
+    geojsonLayer,
+    aidIconLayer, //
     // iconLayer,
-    // cp,
   ];
 
   // マップコンテナのスタイル

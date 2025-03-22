4af3120c9bb9b2721b0102c34f20142981b91aab
sugasaki <sugasaki@gmail.com>
2025-03-22 13:23:51 +0900

    useResultDataフックを修正し、レースデータに基づく位置計算機能を追加。距離計算の最適化とエラーハンドリングを強化。

--- src/hooks/useResultData.ts
+++ src/hooks/useResultData.ts
@@ -1,23 +1,45 @@
 import { useStore } from '../store/store';
+import { getPositionAtDistance } from '../utils/pathUtils';
 // import { createData } from '../utils/raceData';
+import { getDistanceAtTime } from '../utils/timeUtils';
+import { courseData } from '../utils/mapDataLoader';
 
 export const useResultData = () => {
-  const { setPersonLocationData } = useStore();
+  const { raceData, setPersonLocationData } = useStore();
 
   const createResultData = (time: number): any => {
     // const result = createData(raceData, time);
+    // console.log(time, 'time');
+
+    // console.log(raceData, 'raceData');
+    const resultA = raceData[0].results[0];
+    // console.log(resultA, 'resultA');
+
+    const thisTime = time * 2;
+
+    const length = getDistanceAtTime(resultA, thisTime);
+    // console.log(length, 'length');
+
+    if (length === null) {
+      console.error('Calculated length is null');
+      return;
+    }
+
+    const LengthMeter = length * 1000;
+
+    const position = getPositionAtDistance(courseData, LengthMeter);
+    // console.log(position, 'position');
+    if (position === null) {
+      console.error('Calculated length is null');
+      return;
+    }
 
     // サンプルデータ
-    const data = [
-      { position: [139.7528, 35.6852], size: 100 },
-      { position: [139.7628, 35.6952], size: 100 },
-      { position: [139.7428, 35.6752], size: 100 },
-    ];
-
-    console.log(data, 'data');
-    console.log(time, 'time');
+    const data = [{ position: [position[0], position[1]], size: 500 }];
 
     setPersonLocationData(data);
+
+    // setPersonLocationData(data);
   };
 
   return { createResultData };

10be476531c8364f3b84034cbdfc16d27da8442b
sugasaki <sugasaki@gmail.com>
2025-03-28 00:16:03 +0900

    Remove unused Coordinate import from map.ts

--- src/types/map.ts
+++ src/types/map.ts
@@ -1,6 +1,5 @@
 // Define types for map-related data
 
-import { Coordinate } from './geo';
 import { Scatterplot2D } from './scatterplot';
 
 // Export Scatterplot2D as PersonLocation for use in the map store

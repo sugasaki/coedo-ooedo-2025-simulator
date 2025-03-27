8b33273c80feb8f9af9896275886bc4d691780de
sugasaki <sugasaki@gmail.com>
2025-03-27 09:03:46 +0900

    Refactor AnimationFrame component by removing unused props and commented code for improved clarity

--- src/components/AnimationFrame.tsx
+++ src/components/AnimationFrame.tsx
@@ -1,30 +1,13 @@
 import { useStore } from '../store/store';
 
-interface Props {
-  // min: number;
-}
-
-export const AnimationFrame = ({ }: Props) => {
-  // const { isPlaying } = useAnimationFrame({
-  //   min,
-  //   autoStart: false,
-  // });
-
-  const animationFrameValue = useStore(state => state.animationFrameValue);
+export const AnimationFrame = () => {
+  const { animationFrameValue } = useStore();
 
   return (
     <div>
       <div style={{ margin: '0' }} className="text-1x text-gray-500">
         現在のフレーム: {animationFrameValue}
       </div>
-      {/* <div
-        style={{
-          margin: '0',
-          color: isPlaying ? 'green' : 'red',
-        }}
-      >
-        状態: {isPlaying ? '再生中' : '停止中'}
-      </div> */}
     </div>
   );
 };
